32bcac2981a80a12b6a6bd9e05e08d1418bb9f08
mateusz.pierzchala <mateusz.pierzchala1@gmail.com>
2019-02-04 20:36:55 +0100

    log gen przed testami

--- lambda_plural/spark-lambda/src/main/scala/clickstream/LogProducer.scala
+++ lambda_plural/spark-lambda/src/main/scala/clickstream/LogProducer.scala
@@ -23,22 +23,52 @@ object LogProducer extends App {
 
   val rnd = new Random()
   val filePath = wlc.filePath
+  val destPath = wlc.destPath
 
-  val fw = new FileWriter(filePath, true)
+  for (fileCount <- 1 to wlc.numberOfFiles) {
+    val fw = new FileWriter(filePath, true)
 
-  //randomizacja odstępu czasowego kolejnych klików
-  val incrementTimeEvery = rnd.nextInt(wlc.records -1) + 1
-  var timestamp = System.currentTimeMillis()
-  var adjustedTimeStamp = timestamp
+   //randomizacja odstępu czasowego kolejnych klików
+    val incrementTimeEvery = rnd.nextInt(wlc.records -1) + 1
+    var timestamp = System.currentTimeMillis()
+    var adjustedTimeStamp = timestamp
 
-for (iteration <- 1 to wlc.records){
-  adjustedTimeStamp = adjustedTimeStamp + ((System.currentTimeMillis()-timestamp) * wlc.timeMultiplier)
-  timestamp = System.currentTimeMillis()
-  val action = iteration %(rnd.nextInt(200)+1) match {
-    case 0 => "purchase"
-    case 1 => "add_to_cart"
-    case _ => "page_view"
+    for (iteration <- 1 to wlc.records){
+      adjustedTimeStamp = adjustedTimeStamp + ((System.currentTimeMillis()-timestamp) * wlc.timeMultiplier)
+      timestamp = System.currentTimeMillis()
+      val action = iteration %(rnd.nextInt(200)+1) match {
+          case 0 => "purchase"
+          case 1 => "add_to_cart"
+          case _ => "page_view"
+        }
+      val referrer = Referrers(rnd.nextInt(Referrers.length -1))
+      val prevPage = referrer match {
+        case "Internal" => Pages(rnd.nextInt(Pages.length -1))
+        case _ => ""
+      }
+
+      val visitor = Visitors(rnd.nextInt(Visitors.length -1))
+      val page = Pages(rnd.nextInt(Pages.length -1))
+      val product = Products(rnd.nextInt(Products.length -1))
+
+      val line = s"%adjustedTimestamp\t$referrer\t$action\t$prevPage\t$visitor\t$page\t$product\n"
+      fw.write(line)
+
+      if (iteration % incrementTimeEvery ==0) {
+        println(s"Sent $iteration messages!")
+        val sleeping = rnd.nextInt(incrementTimeEvery *60)
+        println(s"Sleeping for $sleeping ms")
+        Thread sleep sleeping 
+      }
+
+    }
+    fw.close()
+
+    val outputFile = FileUtils.getFile(s"${destPath}data_$timestamp")
+    println(s"Moving produced data to $outputFile")
+    FileUtils.moveFile(FileUtils.getFile(filePath), outputFile)
+    val sleeping = 5000
+    println(s"Sleeping for $sleeping ms")
+
   }
-  val referrer = Referrers(rnd)
-}
 }
